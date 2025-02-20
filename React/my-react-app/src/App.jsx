09418a39c1f6a7090cb2489c67c03df9bd799ee0
Krish-Om <krishombasukala@gmail.com>
2025-02-20 11:16:01 +0545

    Refactor App component to introduce Card component and update layout with new styles

--- React/my-react-app/src/App.jsx
+++ React/my-react-app/src/App.jsx
@@ -3,32 +3,27 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const Card = ({ title }) => {
+  return (
+    <div className='cardDesign'>
+      <h2>This is {title}</h2>
+    </div >
+  )
+}
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>Hi! this is Krishom, Hello World</h1>
+      <p className='mainParagraph'>The first day of learning React </p>
+
+
+      <Card title={'Card 1'} />
+      <Card title={'Card 2'} />
+
+    </div>
   )
 }
 
