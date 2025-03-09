4638571666567c268f7d675867f3e9e080f0d17a
Krish-Om <krishombasukala@gmail.com>
2025-03-09 09:55:02 +0545

    Add movieList state and fetch popular movies in Movies component

--- React/learn_router/src/components/movies.jsx
+++ React/learn_router/src/components/movies.jsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { Link } from "react-router-dom";
 
 function Movies(){
-
+    const [movieList, setMovies] = useState([]);
     useEffect(()=>{
 
         const fetchData = async ()=>{
@@ -10,8 +10,8 @@ function Movies(){
                 const response = await fetch("https://api.themoviedb.org/3/movie/popular?api_key=e8049c19d4f7b3d6b01f7502cd306ce0")
                 const moviesData = await response.json();
                 console.log(`Page : ${moviesData.page}`)
-                console.table(moviesData.results)
-                
+                // console.table(moviesData.results)
+                setMovies(moviesData.results)
             } catch (error) {
                 console.log(`Error:${error}`);
             }    
@@ -22,9 +22,16 @@ function Movies(){
     return (
         <>
             <h1>Movies</h1>
+            
             <Link to={"/"}>
                 <button>Home</button>
             </Link>
+
+            <main>
+                <div>
+                    <p></p>
+                </div>
+            </main>
         </>
     )
 }
