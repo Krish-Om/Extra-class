f018c96c58a636bc1b6744fa3536683e36cadba1
Krish-Om <krishombasukala@gmail.com>
2025-03-09 10:47:56 +0545

    Refactor Movies component to display movie cards and handle empty state

--- React/learn_router/src/components/movies.jsx
+++ React/learn_router/src/components/movies.jsx
@@ -1,50 +1,65 @@
 import { useEffect,useState } from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import "./Movies.css"
 
+const imgBaseUrl = "https://image.tmdb.org/t/p/w500";
+const MovieCard= ({movieData})=>{
+    return (
+        <>
+            <div className="movie-card">
+                <img src={`${imgBaseUrl}/${movieData.poster_path}`} alt="" srcset="" />
+                <div className="movie-details">
+                    <h2 className="movie-title">{movieData.title}</h2>
+                    <p className="movie-overview">{movieData.overview}</p>
+                    <p className="movie-release">Rating: {movieData.vote_average}</p>
+                    <p className="movie-popularity">Popularity: {movieData.popularity}</p>
+
+                </div>
+            </div>
+        </>
+    )
+}
 function Movies(){
     const [movieList, setMovies] = useState([]);
     useEffect(()=>{
-
-        const fetchData = async ()=>{
+       (async function fetchData(){
             try {
                 const response = await fetch("https://api.themoviedb.org/3/movie/popular?api_key=e8049c19d4f7b3d6b01f7502cd306ce0")
                 const moviesData = await response.json();
                 console.log(`Page : ${moviesData.page}`)
                 console.log(moviesData.results)
                 setMovies(moviesData.results)
+
+
             } catch (error) {
                 console.log(`Error:${error}`);
             }    
-        };
-        fetchData();
+        })();
     },[]);
 
     return (
         <>
             <h1>Movies</h1>
-            
-            <Link to={"/"}>
-                <button>Home</button>
-            </Link>
+            {
+                movieList.length !== 0?
+                    <div className="movie-container">
+                        {movieList.map((movie) =>{
+                            return (<MovieCard movieData={movie}/>)
+                        })
+                    }
+                    </div>
+                : <>
+                    <h1>No data found!!</h1>
+                </>
+            }
 
-            <main>
-                <div>
-                    <h2>Movies List:</h2>
-                    <ul>
-                        {movieList.map(movie=>(
-                            <li key={movie.id}>
-                                <h3>{movie.title}</h3>
-                                <p><strong>Release Date:</strong>{movie.release_date}</p>
-                                <p><strong>Rating: </strong>{movie.vote_average}</p>
-                                <p>{movie.overview.substring(onabort,15)}...</p>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-            </main>
+            
         </>
     )
 }
+
+
+
+
 
 export default Movies;
