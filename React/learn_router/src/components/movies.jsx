771b653bb3e8099b192e96318f25d318260cc097
Krish-Om <krishombasukala@gmail.com>
2025-03-09 10:10:21 +0545

    Add movie list display and styling in Movies component

--- React/learn_router/src/components/movies.jsx
+++ React/learn_router/src/components/movies.jsx
@@ -1,5 +1,6 @@
-import { useEffect } from "react";
+import { useEffect,useState } from "react";
 import { Link } from "react-router-dom";
+import "./Movies.css"
 
 function Movies(){
     const [movieList, setMovies] = useState([]);
@@ -10,7 +11,7 @@ function Movies(){
                 const response = await fetch("https://api.themoviedb.org/3/movie/popular?api_key=e8049c19d4f7b3d6b01f7502cd306ce0")
                 const moviesData = await response.json();
                 console.log(`Page : ${moviesData.page}`)
-                // console.table(moviesData.results)
+                console.log(moviesData.results)
                 setMovies(moviesData.results)
             } catch (error) {
                 console.log(`Error:${error}`);
@@ -29,7 +30,17 @@ function Movies(){
 
             <main>
                 <div>
-                    <p></p>
+                    <h2>Movies List:</h2>
+                    <ul>
+                        {movieList.map(movie=>(
+                            <li key={movie.id}>
+                                <h3>{movie.title}</h3>
+                                <p><strong>Release Date:</strong>{movie.release_date}</p>
+                                <p><strong>Rating: </strong>{movie.vote_average}</p>
+                                <p>{movie.overview.substring(onabort,15)}...</p>
+                            </li>
+                        ))}
+                    </ul>
                 </div>
             </main>
         </>
